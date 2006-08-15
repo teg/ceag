3de96f47569f54320b7899c232e65d60f41d852d
Tom Gundersen <teg@jklm.no>
2006-08-15 21:35:22 +0000

    only loads map JScripts if explicitly asked for
    git-svn-id: svn://www.jklm.no/ceag@53 cf62ef7f-851a-0410-8a6c-b699277a4589

--- app/controllers/addresse_controller.rb
+++ app/controllers/addresse_controller.rb
@@ -2,4 +2,9 @@ class AddresseController < ApplicationController
   def index
     render :action => 'list'
   end
+  
+  def vis_kart
+    @show_map = true;
+    render :action => 'list'
+  end
 end
